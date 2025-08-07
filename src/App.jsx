8b440637025feefa4b0dedfcd79f687915762e41
Ronit Singh <ronit2001krish@gmail.com>
2025-08-07 14:14:18 +0530

    Added Lazy Loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,39 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import Login from "./pages/Auth/Login";
-import SignUp from "./pages/Auth/SignUp";
-import Dashboard from "./pages/Home/Dashboard";
-import InterviewPrep from "./pages/InterviewPrep/InterviewPrep";
-
 import { Toaster } from "react-hot-toast";
 import UserContextProvider from "./context/userContext";
+import SessionLoader from "./components/Loader/SessionLoader";
+import SpinnerLoader from "./components/Loader/SpinnerLoader";
+
+// Lazy loaded pages
+const LandingPage = lazy(() => import("./pages/LandingPage"));
+const Login = lazy(() => import("./pages/Auth/Login"));
+const SignUp = lazy(() => import("./pages/Auth/SignUp"));
+const Dashboard = lazy(() => import("./pages/Home/Dashboard"));
+const InterviewPrep = lazy(() => import("./pages/InterviewPrep/InterviewPrep"));
 
 function App() {
   return (
     <UserContextProvider>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route
-            path="/interview-prep/:sessionId"
-            element={<InterviewPrep />}
-          />
-        </Routes>
+        <Suspense
+          fallback={
+            <div className="h-screen w-screen bg-black flex justify-center items-center">
+              <SpinnerLoader />
+            </div>
+          }
+        >
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route
+              path="/interview-prep/:sessionId"
+              element={<InterviewPrep />}
+            />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
 
       <Toaster
